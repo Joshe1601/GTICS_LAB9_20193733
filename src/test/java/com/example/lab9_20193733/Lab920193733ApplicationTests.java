d6eebfecaeb545ffac544610a2da9fe020b05c9c
Jose Morillos <jmorillosp@pucp.edu.pe>
2023-10-22 17:29:01 -0500

    Laboratorio iniciado

---
+++ src/test/java/com/example/lab9_20193733/Lab920193733ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab9_20193733;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab920193733ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
